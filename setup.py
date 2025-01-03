7fe78d54f2588e0dd66f67672a36ced8afc85201
Mahmood-Anaam <eng.mahmood.anaam@gmail.com>
2025-01-03 23:11:41 +0300

    update

--- setup.py
+++ setup.py
@@ -8,17 +8,16 @@ setup(
     author_email="eng.mahmood.anaam@gmail.com",
     url="https://github.com/Mahmood-Anaam/arabic-visual-question-answering.git",
     license="MIT",
-    packages=find_packages(where="aravqa"),
-    package_dir={"": "aravqa"},
-    install_requires=[
-        "vinvl@git+https://github.com/Mahmood-Anaam/vinvl.git",
-        "vinvl_bert@git+https://github.com/Mahmood-Anaam/vinvl_bert.git",
-        "Violet@git+https://github.com/Mahmood-Anaam/Violet.git",
-        "yacs",
-        "transformers",
-        "google-generativeai>=0.8.2",
-        
-    ],
+    packages=find_packages(exclude=["notebooks", "assets", "scripts", "tests"]),
+   
+    # install_requires=[
+    #     "vinvl@git+https://github.com/Mahmood-Anaam/vinvl.git",
+    #     "vinvl_bert@git+https://github.com/Mahmood-Anaam/vinvl_bert.git",
+    #     "Violet@git+https://github.com/Mahmood-Anaam/Violet.git",
+    #     "yacs",
+    #     "transformers",
+    #     "google-generativeai>=0.8.2",
+    # ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",

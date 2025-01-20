ca97fec422f2aa872335fe70d49e728933b67a19
Eng.Mahmood Anaam <eng.mahmood.anaam@gmail.com>
2025-01-20 17:06:53 +0300

    Update vqa_v2_dataloader.py

--- aravqa/datasets/vqa_v2_dataloader.py
+++ aravqa/datasets/vqa_v2_dataloader.py
@@ -11,4 +11,56 @@ class VQAv2DataLoader(OKVQADataLoader):
         """
         super().__init__(dataset=dataset,config=config)
 
+     def collate_fn(self, batch):
+        """
+        Custom collate function for preparing the batch data.
+
+        Args:
+            batch (list): A list of samples from the dataset.
+
+        Returns:
+            dict: A dictionary containing batched prompts, answers, images,multiple_choice_answer, and metadata.
+        """
+        
+        result = {
+            "question_id":[],
+            "image_id":[],
+            "prompts":[],
+            "answers":[],
+            "multiple_choice_answer":[],
+        }
+        
+
+        for item in batch:
+            # Create a prompt using filtered captions
+            prompt = self.create_prompt(item["question"], {
+                "bit": item["bit"],
+                "violet": item["violet"]
+            })
+            
+            result["question_id"].append(item["metadata"]["question_id"])
+            result["image_id"].append(item["metadata"]["image_id"])
+            result["prompts"].append(prompt)
+            result["answers"].append(list(map(lambda ans:ans.get("answer",""),item["answers"])))
+            result["multiple_choice_answer"].append(item["multiple_choice_answer"])
+            
+        return result
+                                     
+
+     
+
+    def get_dataloader(self):
+        """
+        Returns a DataLoader object for the dataset.
+
+        Returns:
+            torch.utils.data.DataLoader: The DataLoader instance.
+        """
+        return torch.utils.data.DataLoader(
+            self.dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            collate_fn=self.collate_fn,
+        )
+
 

11b828b488134f405857681a8c72c99816f2d3e9
Mahmood-Anaam <eng.mahmood.anaam@gmail.com>
2025-01-20 21:46:34 +0300

    Done

--- aravqa/datasets/vqa_v2_dataloader.py
+++ aravqa/datasets/vqa_v2_dataloader.py
@@ -1,4 +1,5 @@
 from .okvqa_dataloader import OKVQADataLoader
+import torch
 
 class VQAv2DataLoader(OKVQADataLoader):
     def __init__(self, dataset, config):
@@ -10,4 +11,56 @@ class VQAv2DataLoader(OKVQADataLoader):
             config (Config): The configuration object containing batch size and prompt formatting rules.
         """
         super().__init__(dataset=dataset,config=config)
+    
+    def collate_fn(self, batch):
+        """
+        Custom collate function for preparing the batch data.
+
+        Args:
+            batch (list): A list of samples from the dataset.
+
+        Returns:
+            dict: A dictionary containing batched prompts, answers, images, and metadata.
+        """
+        
+        result = {
+            "question_id":[],
+            "image_id":[],
+            "prompts":[],
+            "answers":[],
+            "multiple_choice_answer":[],
+        }
+        
+
+        for item in batch:
+            # Create a prompt using filtered captions
+            prompt = self.create_prompt(item["question"], {
+                "bit": item["bit"],
+                "violet": item["violet"]
+            })
+            
+            result["question_id"].append(item["metadata"]["question_id"])
+            result["image_id"].append(item["metadata"]["image_id"])
+            result["prompts"].append(prompt)
+            result["answers"].append(list(map(lambda ans:ans.get("answer",""),item["answers"])))
+            result["multiple_choice_answer"].append(item["multiple_choice_answer"])
+        return result
+                                     
+
+     
+
+    def get_dataloader(self):
+        """
+        Returns a DataLoader object for the dataset.
+
+        Returns:
+            torch.utils.data.DataLoader: The DataLoader instance.
+        """
+        return torch.utils.data.DataLoader(
+            self.dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            collate_fn=self.collate_fn,
+        )
+
 
--- aravqa/datasets/vqa_v2_dataset.py
+++ aravqa/datasets/vqa_v2_dataset.py
@@ -1,5 +1,23 @@
-from .okvqa_dataset import OKVQADataset
+import torch
 
-class VQAv2Dataset(OKVQADataset):
+class VQAv2Dataset(torch.utils.data.Dataset):
     def __init__(self, BDS, VDS):
-        super().__init__(BDS=BDS,VDS=VDS)
+        self.BDS = BDS
+        self.VDS = VDS
+       
+
+    def __len__(self):
+        return len(self.BDS)
+
+    def __getitem__(self, idx):
+        example = {
+            "metadata": self.BDS[idx]["metadata"],
+            "image": self.BDS[idx]["image"],
+            "question": self.BDS[idx]["question"],
+            "answers": self.BDS[idx].get("answers",[]),
+            "multiple_choice_answer": self.BDS[idx]["multiple_choice_answer"],
+            "bit": self.BDS[idx].get("captions",[]),
+            "violet": self.VDS[idx].get("captions",[])
+            
+        }
+        return example

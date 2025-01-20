5c4a75331d5769d1e69df22a282a6b28204eb495
Eng.Mahmood Anaam <eng.mahmood.anaam@gmail.com>
2025-01-20 16:09:19 +0300

    Update vqa_v2_dataset.py

--- aravqa/datasets/vqa_v2_dataset.py
+++ aravqa/datasets/vqa_v2_dataset.py
@@ -3,3 +3,15 @@ from .okvqa_dataset import OKVQADataset
 class VQAv2Dataset(OKVQADataset):
     def __init__(self, BDS, VDS):
         super().__init__(BDS=BDS,VDS=VDS)
+
+    def __getitem__(self, idx):
+        example = {
+            "metadata": self.BDS[idx]["metadata"],
+            "image": self.BDS[idx]["image"],
+            "question": self.BDS[idx]["question"],
+            "answers": self.BDS[idx].get("answers",[]),
+            "multiple_choice_answer":self.BDS[idx]["multiple_choice_answer"],
+            "bit": self.BDS[idx].get("captions",[]),
+            "violet": self.VDS[idx].get("captions",[])
+            
+        }
